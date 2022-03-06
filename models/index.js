0660a8ae81b6400c87d093dcddae76c6361d84b6
reggietenk <reggietenkorang@gmail.com>
2022-03-05 21:17:19 -0500

    added routes & models

---
+++ models/index.js
@@ -0,0 +1,48 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require('./Comment')
+
+
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+User.belongsToMany(Post, {
+    foreignKey: 'user_id'
+});
+  
+Post.belongsToMany(User, {
+   foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+
+module.exports = { User, Post, Vote, Comment };
