fc38572544d7e7a8b51fc9a88e9b41be7d01b7f3
reggietenk <reggietenkorang@gmail.com>
2022-03-06 20:35:54 -0500

    added post logic & stylesheet

--- models/index.js
+++ models/index.js
@@ -13,21 +13,21 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-User.belongsToMany(Post, {
-    foreignKey: 'user_id'
-});
+// User.belongsToMany(Comment, {
+//     foreignKey: 'user_id'
+// });
   
-Post.belongsToMany(User, {
-   foreignKey: 'post_id'
-});
+// Post.belongsToMany(User, {
+//    foreignKey: 'post_id'
+// });
 
-User.hasMany(Vote, {
-    foreignKey: 'user_id'
-});
+// User.hasMany(Post, {
+//     foreignKey: 'user_id'
+// });
   
-Post.hasMany(Vote, {
-    foreignKey: 'post_id'
-});
+// Post.hasOne(User, {
+//     foreignKey: 'post_id'
+// });
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
@@ -45,4 +45,4 @@ Post.hasMany(Comment, {
     foreignKey: 'post_id'
   });
 
-module.exports = { User, Post, Vote, Comment };
+module.exports = { User, Post,Comment };
